71fd4c1bc8c744dfef6f9c676b9a76354d889683
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-03-13 20:46:01 +0530

    project setup completed

---
+++ caption-nova-backend/src/main/java/org/backend/captionnovabackend/CaptionNovaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.backend.captionnovabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptionNovaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptionNovaBackendApplication.class, args);
+    }
+
+}
---
+++ caption-nova-backend/src/test/java/org/backend/captionnovabackend/CaptionNovaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.captionnovabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptionNovaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
